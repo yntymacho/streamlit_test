be24853bbf0833cc0fa5ed7422d6c020c8420d7e
yntymacho <80584268+yntymacho@users.noreply.github.com>
2023-10-24 00:11:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
+ импортируем библиотеку streamlit
 import streamlit as st
-import pandas as pd
-import plotly_express as px
 
-PATH = "https://www.dropbox.com/scl/fi/226nfwteim6x2x7q0c36a/football.csv?dl=1&rlkey=cd2t0odbqr8g0yxnnul51mykb"
+# заголовок приложения
+st.title('Калькулятор индекса массы тела (ИМТ)')
 
-df = st.cache_data(pd.read_csv)(PATH)
+# СЧИТЫВАЕМ ВЕС
+weight = st.number_input("Введите ваш вес (в килограммах)")
 
-clubs = st.sidebar.multiselect('Выберите клуб', df['Club'].unique())
-nationalities = st.sidebar.multiselect('Укажите национальность игроков', df['Nationality'].unique())
+# СЧИТЫВАЕМ РОСТ
+# используем radio button, чтобы указать единицы измерения
+status = st.radio('Укажите единицы измерения роста: ', ('см', 'м', 'футы'))
 
-new_df = df[(df['Club'].isin(clubs)) & (df['Nationality'].isin(nationalities))]
-st.write(new_df)
+# сравниваем различные статусы для единиц измерения роста
+if (status == 'см'):
+    # считываем значение роста в сантиметрах
+    height = st.number_input('Сантиметры')
 
-# строим графики используя plotly express
-fig = px.scatter(new_df, x='Overall', y='Age', color='Name')
+    try:
+        bmi = weight / ((height / 100) ** 2)
+    except:
+        st.text("Введите ваш рост")
 
-# рисуем!
-st.plotly_chart(fig)
+elif (status == 'м'):
+    # считываем значение роста в метрах
+    height = st.number_input('Метры')
+
+    try:
+        bmi = weight / (height ** 2)
+    except:
+        st.text("Введите ваш рост")
+
+else:
+    # считываем значение роста в футах
+    height = st.number_input('Футы')
+
+    # 1 meter = 3.28
+    try:
+        bmi = weight / (((height / 3.28)) ** 2)
+    except:
+        st.text("Введите ваш рост")
+
+# проверяем нажата кнопка или нет
+if (st.button('Рассчитать ИМТ')):
+
+    # напечатать значение ИМТ
+    st.text(f"Ваш ИМТ равен {bmi:.2f}")
+
+    # интерпретация ИМТ
+    if (bmi < 16):
+        st.error("Выраженный дефицит массы тела")
+    elif (bmi >= 16 and bmi < 18.5):
+        st.warning("Недостаточная (дефицит) масса тела")
+    elif (bmi >= 18.5 and bmi < 25):
+        st.success("Норма")
+    elif (bmi >= 25 and bmi < 30):
+        st.warning("Избыточная масса тела")
+    elif (bmi >= 30):
+        st.error("Любитель вкусняшек")
